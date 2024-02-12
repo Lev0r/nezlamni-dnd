5fc961a5ba3e5ea3141c8d4a06f75077cd8a9585
Kirill Matienko <kirill.matienko@carrier.com>
2024-02-12 20:58:53 +0200

    char info

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,4 +1,20 @@
 export interface Character {
     name : string;
     age: number;
+    race: string;
+    class: string;
+    level: number;
+
+    str: number;
+    dex: number;
+    con: number;
+    int: number;
+    wis: number;
+    cha: number;
+
+    maxHp: number;
+    curHp: number;
+    tmpHp: number;
+
+    profBonus: number;
 }
